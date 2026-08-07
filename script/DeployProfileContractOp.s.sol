727f2d5e3752caab04e56cff498a18404aeac480
d-democrazy <anggasudarman1993@gmail.com>
2026-08-08 03:31:10 +0700

    Add optimized-contract deploymenst script

---
+++ script/DeployProfileContractOp.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ProfileContractOp} from "../src/ProfileContractOp.sol";
+
+contract DeployProfileContractOp is Script {
+    ProfileContractOp profileContractOp;
+
+    function run() public {
+        string memory explorerUrl = vm.envString("CORESCAN_TESTNET2");
+
+        vm.startBroadcast();
+        profileContractOp = new ProfileContractOp();
+        vm.stopBroadcast();
+
+        console.log(
+            string.concat(
+                "Optimized Profile Contract is deployed to: ", explorerUrl, toString(vm.address(profileContract))
+            )
+        );
+    }
+}
