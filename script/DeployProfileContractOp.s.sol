e05d3125b4beeb539dc5194f07d8e37bd49536a0
d-democrazy <anggasudarman1993@gmail.com>
2026-08-08 03:45:54 +0700

    Typo fixes

--- script/DeployProfileContractOp.s.sol
+++ script/DeployProfileContractOp.s.sol
@@ -17,7 +17,7 @@ contract DeployProfileContractOp is Script {
 
         console.log(
             string.concat(
-                "Optimized Profile Contract is deployed to: ", explorerUrl, toString(vm.address(profileContract))
+                "Optimized Profile Contract is deployed to: ", explorerUrl, toString(vm.address(profileContractOp))
             )
         );
     }
